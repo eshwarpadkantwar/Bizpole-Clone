070c435d296f5c942ce3857c04719f9fd204851d
Eshwar B Padkantwar <ebpadkantwar97@gmail.com>
2024-04-16 21:38:43 +0530

    fourth commit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,7 +5,8 @@ const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => {
+  const toggleNavbar = (e) => {
+    e.preventDefault();
     setIsOpen(!isOpen);
   };
 
@@ -13,10 +14,10 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'white' }}>
     <div className="container-fluid">
       <a className="navbar-brand fw-bold text-dark ms-3" href="#">Bizpole</a>
-      <button className="navbar-toggler bg-danger" onClick={toggleNavbar} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon bg-dark"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarSupportedContent">
+      <button className="navbar-toggler border border-dark" onClick={toggleNavbar} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span className="navbar-toggler-icon bg-danger"></span>
+</button>
+        <div className={`collapse navbar-collapse ${isOpen ? 'show' : 'hidden'}`} id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import Navbar from '../Components/Navbar';
-import Homepage from '../Pages/Homepage';
+// import Homepage from '../Pages/Homepage';
+import Privatelimitedcompany from '../Pages/Privatelimitedcompany';
 import Footer from '../Components/Footer';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Homepage/>
+      <Privatelimitedcompany/>
       <Footer/>
     </div>
   );
